9858036c80caa57e8b65d50e020d3a647e443797
canno <kitax2hayate5109@gmail.com>
2018-01-16 06:22:10 +0900

    hoge

--- record.rb
+++ record.rb
@@ -29,10 +29,6 @@ post '/records' do
   # リクエスト解析
   json = JSON.parse(request.body.read.to_s)
   user_id = json['user_id']
-  file = json['file'].read
-  filename = json['file'].original_filename
-  json['file'] = file
-  json['filename'] = filename
 
   if User.find_by(id: user_id)
     record = Record.new(json)
