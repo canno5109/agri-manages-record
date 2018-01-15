9fcc15f89e617ff01ac0eb0b014c5c29ce719526
canno <kitax2hayate5109@gmail.com>
2018-01-15 15:04:46 +0900

    末尾の$を削除

--- user.rb
+++ user.rb
@@ -1,38 +1,38 @@
-# coding:utf-8$
-require 'active_record'$
-require 'mysql2'$
-require 'sinatra'$
-$
-# DB設定ファイルの読み込み$
-ActiveRecord::Base.configurations = YAML.load_file('database.yml')$
-ActiveRecord::Base.establish_connection(:production)$
+# coding:utf-8
+require 'active_record'
+require 'mysql2'
+require 'sinatra'
 
-class User < ActiveRecord::Base$
-end$
+# DB設定ファイルの読み込み
+ActiveRecord::Base.configurations = YAML.load_file('database.yml')
+ActiveRecord::Base.establish_connection(:production)
 
-# 登録されているユーザーを取得$
-get '/users' do$
-content_type :json, :charset => 'utf-8'$
-users = User.all$
-users.to_json(:root => false) # :root => false は json で返却した時に json の一番上のキーが user にならないようにする$
-end$
+class User < ActiveRecord::Base
+end
+
+# 登録されているユーザーを取得
+get '/users' do
+content_type :json, :charset => 'utf-8'
+users = User.all
+users.to_json(:root => false) # :root => false は json で返却した時に json の一番上のキーが user にならないようにする
+end
 
 get '/' do
   'Hello world!'
 end
 
-# 新規登録$
-post '/users' do$
-# リクエスト解析$
-json = JSON.parse(request.body.read.to_s)$
+# 新規登録
+post '/users' do
+# リクエスト解析
+json = JSON.parse(request.body.read.to_s)
 
-# データ保存$
-  user = User.new(json)$
-  if user.save$
-    { result: "success", code: 200 }.to_json$
-    status 200$
-  else$
-    { result: "failure", code: 400 }.to_json$
-    status 400$
-  end$
+# データ保存
+  user = User.new(json)
+  if user.save
+    { result: "success", code: 200 }.to_json
+    status 200
+  else
+    { result: "failure", code: 400 }.to_json
+    status 400
+  end
 end
